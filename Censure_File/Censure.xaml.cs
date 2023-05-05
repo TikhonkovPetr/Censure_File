715a02505082e0ff56804fd3bffc5bf87a0b08e7
PT <tikhonkovpetr@mail.ru>
2023-05-05 08:40:21 +0300

    Добавьте файлы проекта.

---
+++ Censure_File/Censure.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Censure_File
+{
+    /// <summary>
+    /// Логика взаимодействия для Censure.xaml
+    /// </summary>
+    public partial class Censure : Window
+    {
+        Thread thread;
+        List<object> list = new List<object>();
+        int obsh_colvo_zap = 0;
+        public Censure()
+        {
+            InitializeComponent();
+            List<string> Zapret = new List<string> { "yes", "fuck" };
+            string str = "";
+            string h = "";
+            int number_path = 0;
+            int per = 0;
+            thread = new Thread(NachCensure);
+            list.Add(MainWindow.censure_word);
+            list.Add(str);
+            list.Add(h);
+            list.Add(MainWindow.List_path);
+            list.Add(number_path);
+            BT2.IsEnabled = false;
+            thread.Start(list);
+        }
+        public void NachCensure(object obj)
+        {
+            for (int i = 0; i < ((obj as List<object>)[3] as List<string>).Count(); i++)
+            {
+                Thread thread2 = new Thread(Proverka);
+                Progress2.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(() => Progress2.Value = 0));
+                Progress1.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(() => Progress1.Value += 100 / ((obj as List<object>)[3] as List<string>).Count()));
+                thread2.Start(obj);
+                thread2.Join();
+                if(i!= ((obj as List<object>)[3] as List<string>).Count()-1)     (obj as List<object>)[4] =i + 1;
+                Thread.Sleep(100);
+            }
+            string path = ((obj as List<object>)[3] as List<string>)[Convert.ToInt32((obj as List<object>)[4].ToString())];
+            for (int i = path.Count() - 1; i > 1; i--)
+            {
+                if (path[i] == '\\')
+                {
+                    break;
+                }
+                path = path.Remove(path.Count() - 1);
+            }
+            using (StreamWriter StrWrite = new StreamWriter(path + "Obsh_Zap.txt"))
+            {
+                StrWrite.WriteLine($"Общее количество запрещённых слов {obsh_colvo_zap}");
+            }
+            Thread.Sleep(500);
+            this.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(() => this.Close()));
+        }
+        public void Proverka(object obj)
+        {
+            string path = ((obj as List<object>)[3] as List<string>)[Convert.ToInt32((obj as List<object>)[4].ToString())];
+            for (int i = path.Count() - 1; i > 1; i--)
+            {
+                if (path[i] == '.')
+                {
+                    path = path.Remove(path.Count() - 1);
+                    break;
+                }
+                path = path.Remove(path.Count() - 1);
+            }
+            using (StreamReader StrRead = new StreamReader(path+".txt"))
+            {
+                (obj as List<object>)[1] = StrRead.ReadToEnd();
+            }
+            (obj as List<object>)[1] = ((obj as List<object>)[1] as string).ToLower();
+            string text = ((obj as List<object>)[1] as string);
+            string word = "";
+            int numZap = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ' || text[i] == '\n' || text[i] == '\0' || text[i] == '.' || text[i]=='\r' ||text.Count() == i + 1)
+                {
+                    if (text.Count() == i + 1)
+                    {
+                        word = word + text[i];
+                    }
+                    word = word.ToLower();
+                    foreach (string s in ((obj as List<object>)[0] as List<string>))
+                    {
+                        if(word==s)
+                        {
+                            numZap++;
+                        }
+                    }
+                    word = "";
+                }
+                else
+                {
+                    word = word + text[i];
+                }
+            }
+            obsh_colvo_zap += numZap;
+            foreach (string s in ((obj as List<object>)[0] as List<string>))
+            {
+                Progress2.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,new Action(() => Progress2.Value += 100 / ((obj as List<object>)[0] as List<string>).Count()));
+                (obj as List<object>)[1] = ((obj as List<object>)[1] as string).Replace(s, "******");
+            }
+            using (StreamWriter StrWrite = new StreamWriter(path + "_CountZapret.txt"))
+            {
+                StrWrite.WriteLine($"Количество запрещённых слов {numZap}");
+            }
+            using (StreamWriter StrWrite = new StreamWriter(path + "_Itog.txt"))
+            {
+                StrWrite.WriteLine((obj as List<object>)[1]);
+            }
+            using (StreamReader StrRead = new StreamReader(path + "_Itog.txt"))
+            {
+                (obj as List<object>)[2] = StrRead.ReadToEnd();
+            }
+        }
+        private void Button_Click_END(object sender, RoutedEventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Suspended)
+            {
+            thread.Resume();
+            }
+            thread.Abort();
+            this.Close();
+        }
+
+        private void Button_Click_START(object sender, RoutedEventArgs e)
+        {
+            thread.Resume();
+            BT1.IsEnabled= true;
+            BT2.IsEnabled= false;
+        }
+
+        private void Button_Click_STOP(object sender, RoutedEventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Running || thread.ThreadState == ThreadState.WaitSleepJoin)
+            {
+                thread.Suspend();
+                BT1.IsEnabled = false;
+                BT2.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ Censure_File/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Censure_File
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        public static string path_File = "";
+        public static List<string> censure_word = new List<string>();
+        public static List<string> List_path = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            openFileDialog.Filter = "Текстовые файлы|*.txt";
+        }
+
+        private void Button_Click_Prov(object sender, RoutedEventArgs e)
+        {
+            Censure censure =new Censure();
+            censure.ShowDialog();
+        }
+        private void Button_Click_Vibr(object sender, RoutedEventArgs e)
+        {
+            //Path.Text = openFileDialog.FileName
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path_File = openFileDialog.FileName;
+                for (int i = path_File.Count() - 1; i > 1; i--)
+                {
+                    if (path_File[i] == '\\') break;
+                    path_File = path_File.Remove(path_File.Count() - 1);
+                }
+                Path.Text = path_File;
+                DirSearchFile(path_File);
+            }
+        }
+        private void DirSearchFile(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                foreach (var item in dir.GetFiles())
+                {
+                    if (item.Extension == ".txt")
+                    {
+                        List_path.Add(item.FullName);
+                    }
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void TxtBox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        { 
+        }
+
+        private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string word = "";
+            censure_word.Clear();
+            for (int i = 0; i < TxtBox.Text.Count(); i++)
+            {
+                if (TxtBox.Text[i] == ' ' || TxtBox.Text[i] == '\n' || TxtBox.Text[i] == '\0' || TxtBox.Text[i] == '.' || TxtBox.Text[i] == '\r' || TxtBox.Text.Count() == i + 1)
+                {
+                    if (TxtBox.Text.Count() == i + 1)
+                    {
+                        word = word + TxtBox.Text[i];
+                    }
+                    word = word.ToLower();
+                    censure_word.Add(word);
+                    word = "";
+                }
+                else
+                {
+                    word = word + TxtBox.Text[i];
+                }
+            }
+        }
+    }
+}
